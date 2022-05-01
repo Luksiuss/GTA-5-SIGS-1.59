86c33176971a2b1541c3294d7ded262546cbe84c
Luksiuss <80646077+Luksiuss@users.noreply.github.com>
2022-05-01 17:20:02 +0200

    Add files via upload

---
+++ GTASigs.cpp
@@ -0,0 +1,5 @@
+World - 48 8B 05 ? ? ? ? 45 ? ? ? ? 48 8B 48 08 48 85 C9 74 07
+ReplayInterface - 48 8B 05 ? ? ? ? 41 8B 1E
+Viewport - 48 8B 15 ? ? ? ? 48 8D 2D ? ? ? ? 48 8B CD
+Ammo - 41 2B D1 E8
+Clip - 41 2B C9 3B C8 0F
